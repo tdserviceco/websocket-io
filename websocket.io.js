5214e6bda04bfaf6fa485c5502a204af128a2390
Tommy Danielsson <tdserviceco@gmail.com>
2020-09-28 16:15:34 +0200

    added an API

--- websocket.io.js
+++ websocket.io.js
@@ -1,25 +1,81 @@
-const express = require('express')
+require('dotenv').config()
+
+const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cookie: true
 });
+const MongoClient = require('mongodb').MongoClient;
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log('server is running')
-});
+// Connection URL
+const url = process.env.MONGODB;
+const dbName = 'xboxfighters';
+// Use connect method to connect to the Server
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('Connected')
   socket.on('disconnect', (reason) => {
     if (reason === 'io server disconnect') {
       // the disconnection was initiated by the server, you need to reconnect manually
       socket.connect();
+      console.log('Re-connected')
     }
     console.log('user disconnected');
   });
+  socket.on('server', (data) => {
+    console.log(data)
+    socket.send('client', { value: 'Hi Client!' })
+  })
+
 })
+
+app.use(express.static('public'))
+
+// API
+
+app.get('/api/countries', (req, res) => {
+  var url = process.env.MONGODB;
+  const client = new MongoClient(url, { useUnifiedTopology: true }); // { useUnifiedTopology: true } removes connection warnings;
+  const dbName = "xboxfighters";
+  client
+    .connect()
+    .then(
+      client =>
+        client
+          .db(dbName).
+          collection("Countries").find({})
+          .toArray() // Returns a promise that will resolve to the list of the collections
+    )
+    .then(cols => res.json(cols))
+    .finally(() => client.close());
+});
+
+
+
+app.get('/api/players', (req, res) => {
+  var url = process.env.MONGODB;
+  const client = new MongoClient(url, { useUnifiedTopology: true }); // { useUnifiedTopology: true } removes connection warnings;
+  const dbName = "xboxfighters";
+  client
+    .connect()
+    .then(
+      client =>
+        client
+          .db(dbName).
+          collection("Players").find({})
+          .toArray() // Returns a promise that will resolve to the list of the collections
+    )
+    .then(cols => res.json(cols))
+    .finally(() => client.close());
+});
+
+
+app.post('/api/players', (req, res) => {
+
+})
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log('server is running on ' + process.env.PORT)
+});
