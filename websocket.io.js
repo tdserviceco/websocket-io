d61cc3d02378277b78648374493f5b2209d82718
Tommy Danielsson <tdserviceco@gmail.com>
2020-10-03 16:48:16 +0200

    fixed the issue with socket.io

--- websocket.io.js
+++ websocket.io.js
@@ -8,26 +8,28 @@ const io = require('socket.io')(http, {
 });
 
 this.state = {
-  Countries: __dirname + "/json/Countries.json",
+  // Countries: __dirname + "/json/Countries.json",
   Players: __dirname + "/json/Players.json"
 }
 
-const { Countries,Players } = this.state;
+const { Countries, Players } = this.state;
 
 app.use(cors(), express.static('public'))
 
 // API
 
-app.get('/', (req,res) => {
-  res.send({response: 'Welcome'}).status(200)
+app.get('/', (req, res) => {
+  res.send({ response: 'Welcome' }).status(200)
 })
 
+/* 
 app.get('/api/countries', (req, res) => {
   res.sendFile(Countries);
 })
+ */
 
 app.get('/api/', (req, res) => {
-  res.send({response: 'Yes im still kicking'}).status(200)
+  res.send({ response: 'Yes im still kicking' }).status(200)
 })
 
 app.get('/api/players', (req, res) => {
@@ -40,7 +42,35 @@ app.get('/api/player/?:id&:player&:code', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-  console.log('Connected')
+  const { id } = socket.client;
+  console.log(`User Connected: ${id}`);
+
+  socket.on("player", ({ id, name, code }) => {
+    if (id === 'player-1') {
+      io.emit("player1name", { name })
+      io.emit("player1country", {
+        country: code + ".png"
+      })
+    }
+
+    if (id === 'player-2') {
+      io.emit("player2name", { id, name })
+      io.emit("player2country", {
+        country: code + ".png"
+      })
+    }
+  })
+
+  socket.on("playerScore", ({ player, replaceScoreP1, replaceScoreP2 }) => {
+    if (player === 'player-1') {
+      io.emit("player1Score", { replaceScoreP1 })
+    }
+
+    if (player === 'player-2') {
+      io.emit("player2Score", { replaceScoreP2 })
+    }
+  })
+
   socket.on('disconnect', (reason) => {
     if (reason === 'io server disconnect') {
       // the disconnection was initiated by the server, you need to reconnect manually
@@ -49,11 +79,6 @@ io.on('connection', (socket) => {
     }
     console.log('user disconnected');
   });
-  socket.on('server', (data) => {
-    console.log(data)
-    socket.emit('player', data)
-  })
-
 })
 
 
