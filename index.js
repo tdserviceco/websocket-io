b9a8b74506cfcc7a82fcce5eca036e8e185d84c9
Tdserviceco <tdserviceco@gmail.com>
2022-02-25 22:59:30 +0100

    working...

--- index.js
+++ index.js
@@ -32,64 +32,64 @@ app.get('/api/v1/flags', (req, res) => {
   res.json(flags).status(200)
 })
 
-// io.on('connection', (socket) => {
-//   socket.on('disconnect', (reason) => {
-//     if (reason === 'io server disconnect') {
-//       // the disconnection was initiated by the server, you need to reconnect manually
-//       socket.connect();
-//       console.log('Re-connected')
-//     }
-//     console.log('user disconnected');
-//   });
+io.on('connection', (socket) => {
+  //   socket.on('disconnect', (reason) => {
+  //     if (reason === 'io server disconnect') {
+  //       // the disconnection was initiated by the server, you need to reconnect manually
+  //       socket.connect();
+  //       console.log('Re-connected')
+  //     }
+  //     console.log('user disconnected');
+  //   });
 
-//   socket.on('player-one-name', (name) => {
-//     console.log('name p1:', name);
-//     io.emit('player-one-name', name)
-//   });
+  //   socket.on('player-one-name', (name) => {
+  //     console.log('name p1:', name);
+  //     io.emit('player-one-name', name)
+  //   });
 
-//   socket.on('player-two-name', (name) => {
-//     console.log('name p2:', name);
-//     io.emit('player-two-name', name)
-//   });
+  //   socket.on('player-two-name', (name) => {
+  //     console.log('name p2:', name);
+  //     io.emit('player-two-name', name)
+  //   });
 
-//   socket.on('player-one-score', (score) => {
-//     console.log('score p1:', score)
-//     io.emit('player-one-score', score)
-//   })
+  //   socket.on('player-one-score', (score) => {
+  //     console.log('score p1:', score)
+  //     io.emit('player-one-score', score)
+  //   })
 
-//   socket.on('player-two-score', (score) => {
-//     console.log('score p2:', score)
-//     io.emit('player-two-score', score)
+  //   socket.on('player-two-score', (score) => {
+  //     console.log('score p2:', score)
+  //     io.emit('player-two-score', score)
 
-//   })
+  //   })
 
-//   socket.on('players-swap-side', (swap) => {
-//     console.log('swap:', swap)
-//     io.emit('players-swap-side', swap)
-//   })
+  //   socket.on('players-swap-side', (swap) => {
+  //     console.log('swap:', swap)
+  //     io.emit('players-swap-side', swap)
+  //   })
 
-//   socket.on('round-announce', (announce) => {
-//     console.log('announce:', announce)
-//     io.emit('round-announce', announce)
+  //   socket.on('round-announce', (announce) => {
+  //     console.log('announce:', announce)
+  //     io.emit('round-announce', announce)
 
-//   })
+  //   })
 
-//   socket.on('player-one-country-flag', (flag) => {
-//     console.log('flag p1:', flag)
-//     io.emit('player-one-country-flag', flag)
-//   })
+  //   socket.on('player-one-country-flag', (flag) => {
+  //     console.log('flag p1:', flag)
+  //     io.emit('player-one-country-flag', flag)
+  //   })
 
-//   socket.on('player-two-country-flag', (flag) => {
-//     console.log('flag p2:', flag)
-//     io.emit('player-two-country-flag', flag)
-//   })
+  //   socket.on('player-two-country-flag', (flag) => {
+  //     console.log('flag p2:', flag)
+  //     io.emit('player-two-country-flag', flag)
+  //   })
 
-//   socket.on('activate-xbox-logo', (choice) => {
-//     console.log('activate xbox logo:', choice)
-//     io.emit('activate-xbox-logo', choice)
-//   })
+  //   socket.on('activate-xbox-logo', (choice) => {
+  //     console.log('activate xbox logo:', choice)
+  //     io.emit('activate-xbox-logo', choice)
+  //   })
 
 
-// });
+});
 
 http.listen(process.env.PORT || 3000, () => { console.log('listening on *:3000'); });
