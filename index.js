bb27dc7777bc5f858af764e008e5999b6e7f283e
Tdserviceco <tdserviceco@gmail.com>
2022-02-25 22:35:27 +0100

    trying

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.options('*', cors()) // include before other routes
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    "statusText": "Helloworld"
+  })
+})
+
 app.get('/api/', (req, res) => {
   res.send({ response: 'Yes im still kicking' }).status(200)
 })
