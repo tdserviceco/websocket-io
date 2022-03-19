4d5e55a2c9ebcaff83be23d26f7d7a011d587ca8
Tommy Danielsson <tdserviceco@gmail.com>
2022-03-19 10:01:17 +0100

    added timeout and how many second a interval should be

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const io = new Server(http, {
   cors: {
     origin: '*'
-  }
+  },
+  pingInterval: 10000,
+  pingTimeout: 5000,
 });
 const fs = require('fs');
 const path = require('path');
