25e6b337248b89cd9c97108a0eaab7d58789e4f6
Tommy Danielsson <tdserviceco@gmail.com>
2022-03-20 01:33:24 +0100

    trying a new way

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const cors = require('cors');
 const io = require('socket.io')(http, {
   cors: {
-    origin: "*",
+    origin: ["https://scorehandler.netlify.app", "https://scorehandler.netlify.app/dashboard"],
   }
 });
 const fs = require('fs');
@@ -42,54 +42,54 @@ io.on('connection', (socket) => {
 
   socket.on('player-one-name', (name) => {
     console.log('name p1:', name);
-    io.emit('player-one-name', name)
+    io.emit('player-one-name-answer', name)
   });
 
   socket.on('player-two-name', (name) => {
     console.log('name p2:', name);
-    io.emit('player-two-name', name)
+    io.emit('player-two-name-answer', name)
   });
 
   socket.on('player-one-score', (score) => {
     console.log('score p1:', score)
-    io.emit('player-one-score', score)
+    io.emit('player-one-score-answer', score)
   })
 
   socket.on('player-two-score', (score) => {
     console.log('score p2:', score)
-    io.emit('player-two-score', score)
+    io.emit('player-two-score-answer', score)
 
   })
 
   socket.on('players-swap-side', (swap) => {
     console.log('swap:', swap)
-    io.emit('players-swap-side', swap)
+    io.emit('players-swap-side-answer', swap)
   })
 
   socket.on('round-announce', (announce) => {
     console.log('announce:', announce)
-    io.emit('round-announce', announce)
+    io.emit('round-announce-answer', announce)
 
   })
 
   socket.on('player-one-country-flag', (flag) => {
     console.log('flag p1:', flag)
-    io.emit('player-one-country-flag', flag)
+    io.emit('player-one-country-flag-answer', flag)
   })
 
   socket.on('player-two-country-flag', (flag) => {
     console.log('flag p2:', flag)
-    io.emit('player-two-country-flag', flag)
+    io.emit('player-two-country-flag-answer', flag)
   })
 
   socket.on('activate-xbox-logo', (choice) => {
     console.log('activate xbox logo:', choice)
-    io.emit('activate-xbox-logo', choice)
+    io.emit('activate-xbox-logo-answer', choice)
   })
 
   socket.on('layout-skin', (choice) => {
     console.log('layout-skin:', choice)
-    io.emit('layout-skin', choice)
+    io.emit('layout-skin-answer', choice)
   })
 });
 
